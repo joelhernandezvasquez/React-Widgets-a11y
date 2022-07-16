3cbba25c740c07aa4c3e688b343dffb40dd19bee
Joel Hernandez <jhernandez98@outlook.es>
2022-07-16 14:52:04 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import CheckboxTristate from './components/CheckboXTristate/CheckboxTristate';
+//import AlertContainer from './components/Alert/AlertContainer';
+// import ToogleButton from './components/ToogleButton/ToogleButton';
+
+
+function App() {
+  return (
+    <div className="App">
+   <CheckboxTristate/>
+    </div>
+  );
+}
+
+export default App;
